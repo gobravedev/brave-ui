2b4933c956c697fd1759df9d7f9e8981bbc66d2a
wangyang <1749748955@qq.com>
2025-10-10 08:25:10 +0800

    update

--- src/pages/analysis-report/index.tsx
+++ src/pages/analysis-report/index.tsx
@@ -16,7 +16,23 @@ const AnalysisReport: FC<any> = () => {
     //     const resp = await axios.post("/find-pipeline", { component_id: componentId })
     //     setComponents(resp.data)
     // }
-
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [top, setTop] = useState<any>(null);
+    const updateHeight = () => {
+        if (containerRef.current) {
+            const height = containerRef.current.getBoundingClientRect().top // 包含 padding
+            setTop(height);
+        }
+    }
+    useEffect(() => {
+        updateHeight(); // 初始化
+        window.addEventListener("resize", updateHeight);
+        // window.addEventListener("scroll", updateHeight);
+        return () => {
+            window.removeEventListener("resize", updateHeight);
+            //   window.removeEventListener("scroll", updateHeight);
+        };
+    }, []);
     const loadData = async () => {
         setLoading(true)
         // ?analysis_method=${analysisMethod}&project=${project}
@@ -46,12 +62,25 @@ const AnalysisReport: FC<any> = () => {
 
         {/* <div style={{ marginBottom: "1rem" }}></div> */}
 
-        <Row>
-            <Col lg={6} sm={6} xs={24} >
+        <Row style={{ marginTop: "1rem" }}>
+            <Col lg={6} sm={6} xs={24} ref={containerRef} style={{
+
+                position: "sticky",
+                top: `${top}px`, // 吸顶距离
+                alignSelf: "flex-start", // 避免被stretch
+                height: `calc(100vh - ${top}px - 1rem )`, // 可选：固定高度，让内部滚动
+            }}>
                 <Card
                     loading={loading}
                     title={projectObj?.project_name}
                     size="small"
+                    style={{ height: "100%" }}
+                    styles={{
+                        body: {
+                            height: "90%",
+                            overflowY: "auto"
+                        }
+                    }}
                     extra={
                         <Flex gap={"small"}>
                             <Button size="small" color="cyan" variant="solid" onClick={loadData}>刷新</Button>
@@ -103,7 +132,7 @@ export default AnalysisReport
 
 // const ComponentsView:FC<any> = ({components})=>{
 //     return <>
-    
+
 //     </>
 // }
 const LeftPanel: FC<any> = ({ treeData, defaultSelectKey, onSelect: onSelect_ }) => {
@@ -152,14 +181,42 @@ const LeftPanel: FC<any> = ({ treeData, defaultSelectKey, onSelect: onSelect_ })
         },
     ];
     const [selectedKey, setSelectedKey] = useState<any>(defaultSelectKey)
+    const [expandedKeys, setExpandedKeys] = useState<string[]>([]);
+
+    // 找到 key 的所有父节点
+    const getParentKeys = (key: any, nodes: TreeDataNode[], parents: string[] = []): string[] => {
+        for (const node of nodes) {
+            if (node.key === key) return parents;
+            if (node.children) {
+                const res = getParentKeys(key, node.children, [...parents, node.key as string]);
+                if (res.length) return res;
+            }
+        }
+        return [];
+    };
+
+    useEffect(() => {
+        if (defaultSelectKey) {
+            const keys = getParentKeys(defaultSelectKey, treeData);
+            setExpandedKeys(keys);
+            setSelectedKey(defaultSelectKey);
+        }
+    }, [defaultSelectKey, treeData]);
 
     const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
         // console.log('selected', selectedKeys, info);
         if (selectedKeys.length > 0) {
             onSelect_(info)
             setSelectedKey(selectedKeys[0])
+
+            // 选中时展开父节点
+            const keys = getParentKeys(selectedKeys[0], treeData);
+            setExpandedKeys(keys);
         }
 
+    };
+    const onExpand = (keys: React.Key[]) => {
+        setExpandedKeys(keys as string[]);
     };
 
     return (
@@ -168,8 +225,11 @@ const LeftPanel: FC<any> = ({ treeData, defaultSelectKey, onSelect: onSelect_ })
             <Tree
                 selectedKeys={[selectedKey]}
                 showLine
+                expandedKeys={expandedKeys}
+                onExpand={onExpand}
+                autoExpandParent={true} // 自动展开父节点
                 switcherIcon={<DownOutlined />}
-                defaultExpandAll
+                // defaultExpandAll
                 // defaultExpandedKeys={['0-0-0']}
                 onSelect={onSelect}
                 treeData={treeData}
