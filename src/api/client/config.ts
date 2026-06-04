82a768b00f332b38e5f7097e3a18c7e7a67130ed
wangyang <wangyangbioinfo@gmail.com>
2026-06-04 09:21:53 +0800

    udpate

--- src/api/client/config.ts
+++ src/api/client/config.ts
@@ -1,5 +1,8 @@
+import { getPathname } from "@/utils/utils";
+
 export const API_CONFIG = {
-	baseURL: import.meta.env.VITE_API_BASE_URL ?? "/api/v1",
+	// baseURL: import.meta.env.VITE_API_BASE_URL ?? "/api/v1",
+	baseURL: getPathname() + "/api/v1",
 	timeout: 15_000,
 	useMock: import.meta.env.VITE_USE_MOCK === "true",
 	// import.meta.env.DEV || 
